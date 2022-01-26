bbd115acb3ef889f8e5237138f115c4d67913f95
Ivan Kaptue <ivanokaptue@gmail.com>
2022-01-25 22:37:42 -0500

    Update test

---
+++ src/test/java/com/klid/githooksspringboot/ExampleTest.java
@@ -0,0 +1,18 @@
+package com.klid.githooksspringboot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Ivan Kaptue
+ */
+public class ExampleTest {
+
+    @Test
+    public void test_addition() {
+        var result = 6 + 7;
+
+        Assertions.assertEquals(15, result);
+    }
+
+}
